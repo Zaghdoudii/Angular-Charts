bcb1571c9d8e0051e48c8b7b1823837ca2e137a3
Zaghdoudii <mohamed.zgh28@gmail.com>
2023-03-12 00:25:02 +0100

    add cost-project

--- src/app/components/chart1/chart1.component.ts
+++ src/app/components/chart1/chart1.component.ts
@@ -17,42 +17,40 @@ export class Chart1Component implements OnInit {
   constructor(private service: ChartServiceService) { }
 
   chartdata: any;
-  labeldata: any[] = [];
-  realdata: any[] = [];
+  nameAccData: any[] = [];
+  costData: any[] = [];
   projectdata: any[] = [];
   perioddata: any[] = [];
   accountName: any[] = [];
   period: any[] = [];
   sum: any = 0;
-
+  mostCostProject: any = 0;
   ngOnInit(): void {
     this.service.Getchartinfo().subscribe(result => {
       this.chartdata = result;
       if (this.chartdata != null) {
         for (let i = 0; i < this.chartdata.length; i++) {
-          //  console.log(this.chartdata[i]);
-          this.labeldata.push(this.chartdata[i].account_name);
-          this.realdata.push(this.chartdata[i].cost);
+          this.nameAccData.push(this.chartdata[i].account_name);
+          this.costData.push(this.chartdata[i].cost);
           this.projectdata.push(this.chartdata[i].projet_id);
           this.perioddata.push(this.chartdata[i].period_end);
         }
 
 
-        let accountName = Array.from(new Set(this.labeldata));
-        let totalCost = accountName.map(type => this.realdata.reduce((acc, curr, index) => this.labeldata[index] === type ? acc + curr : acc, 0));
+        let accountName = Array.from(new Set(this.nameAccData));
+        let totalCost = accountName.map(type => this.costData.reduce((acc, curr, index) => this.nameAccData[index] === type ? acc + curr : acc, 0));
 
         let idProject = Array.from(new Set(this.projectdata));
-        let totalCostProject = idProject.map(type => this.realdata.reduce((acc, curr, index) => this.projectdata[index] === type ? acc + curr : acc, 0));
+        let totalCostProject = idProject.map(type => this.costData.reduce((acc, curr, index) => this.projectdata[index] === type ? acc + curr : acc, 0));
 
 
         let period = Array.from(new Set(this.perioddata));
-        // console.log("Period: ", period);
-        let totalCostPeriod = period.map(type => this.realdata.reduce((acc, curr, index) => this.perioddata[index] === type ? acc + curr : acc, 0));
+        let totalCostPeriod = period.map(type => this.costData.reduce((acc, curr, index) => this.perioddata[index] === type ? acc + curr : acc, 0));
 
-
+        this.mostCostProject = Math.max(...totalCostProject);
         let sum1 = 0;
-        for (let i = 0; i < this.realdata.length; i++) {
-          sum1 += this.realdata[i];
+        for (let i = 0; i < this.costData.length; i++) {
+          sum1 += this.costData[i];
         }
 
         this.sum = sum1;
@@ -62,9 +60,9 @@ export class Chart1Component implements OnInit {
         this.RenderChart(accountName, totalCost, 'barchart', 'bar');
         this.RenderChart(idProject, totalCostProject, 'piechart1', 'pie');
         this.RenderChart(period.sort(), totalCostPeriod, 'linechart', 'line');
-        // this.RenderChart(this.labeldata, this.realdata, 'polarareachart', 'polarArea');
-        // this.RenderChart(this.labeldata, this.realdata, 'scatterchart', 'scatter');
-        // this.RenderChart(this.labeldata, this.realdata, 'Bubblechart', 'bubble');
+        // this.RenderChart(this.nameAccData, this.costData, 'polarareachart', 'polarArea');
+        // this.RenderChart(this.nameAccData, this.costData, 'scatterchart', 'scatter');
+        // this.RenderChart(this.nameAccData, this.costData, 'Bubblechart', 'bubble');
       }
 
 
@@ -73,11 +71,11 @@ export class Chart1Component implements OnInit {
   }
 
 
-  RenderChart(labeldata: any, maindata: any, id: any, type: any) {
+  RenderChart(nameAccData: any, maindata: any, id: any, type: any) {
     const myChart = new Chart(id, {
       type: type,
       data: {
-        labels: labeldata,
+        labels: nameAccData,
         datasets: [{
           label: 'Total cost ',
           data: maindata,
